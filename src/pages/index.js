8f3149f7b053439ebe08c9d49f4de01f3c4a84cd
gonza <gonzalo.muscolo@gmail.com>
2024-11-11 17:36:46 -0300

    adding ping

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,29 @@
 import Link from "next/link";
+import axios from 'axios';
+const apiUrl = process.env.NEXT_PUBLIC_API_URL;
 
 export default function Home() {
+  
+  const ping = async () => {
+    console.log(apiUrl+'ping')
+  // Realizar la solicitud GET al endpoint de Rails
+    axios.get(apiUrl+'ping')
+    .then((res) => {
+      // Guardar la respuesta en el estado
+      console.log(res.data.message);
+    })
+    .catch((error) => {
+      console.error("Hubo un error al hacer la solicitud:", error);
+    });
+  }
+
   return (
     <div>
       Hello World.{" "}
       <Link href="/about">
         About
       </Link>
+      <button onClick={ping}>ping asd</button>
     </div>
   );
 }
