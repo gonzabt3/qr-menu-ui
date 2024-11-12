c71deaac7f6a1ac0ed1370da3290ddbce0e70760
gonza <gonzalo.muscolo@gmail.com>
2024-11-12 15:56:22 -0300

    extract varaibles of auth0

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,23 +1,21 @@
 'use client'
 import React, { useEffect, useState } from "react";
 import { AppProps } from "next/app";
-
 import { Auth0Provider } from "@auth0/auth0-react";
 
+const auth0Domain = process.env.AUTH_DOMAIN;
+const auth0ClientId = process.env.AUTH_CLIENT_ID;
 export default function App({ Component, pageProps }:AppProps) {
   const [isClient, setIsClient] = useState(false)
  
   useEffect(() => {
-    
     setIsClient(true)
-    console.log("sad")
-    console.log(window.location.origin)
   }, [])
   return (<>
     { isClient ?
     <Auth0Provider
-    domain={'dev-uqe3iibrr3fdelk0.us.auth0.com'}
-    clientId={'GIMjZUPohy1Uvv8YAXGwfQ5VyqPhV8AH'}
+    domain={auth0Domain}
+    clientId={auth0ClientId}
     authorizationParams={{ redirect_uri: window.location.origin }}
   >
     <Component {...pageProps} />
