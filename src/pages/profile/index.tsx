0d6c7f71017e4208dba8296d7523bd46c876f50e
gonza <gonzalo.muscolo@gmail.com>
2024-12-13 17:37:21 -0300

    add more code

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -5,20 +5,23 @@ import BaseCompents from "../components/BaseCompents";
 import { useAuth0 } from "@auth0/auth0-react";
 import { useRouter } from "next/router";
 import {
+  Button,
   CardRoot,
   GridItem,
   Heading,
  useDisclosure,
 } from "@chakra-ui/react";
 import ProfileInfoDialog from "./ProfileInfoDialog";
+import axios from "axios";
+const apiUrl = process.env.NEXT_PUBLIC_API_URL;
 
 
 const Profile = () => {
-  const { isAuthenticated, loginWithRedirect, user, isLoading } = useAuth0();
+  const { isAuthenticated, loginWithRedirect, user, isLoading, getAccessTokenSilently } = useAuth0();
   const router = useRouter();
   const { isOpen, onOpen, onClose } :any = useDisclosure(); // Controla el estado del modal
   const [inputValue, setInputValue] :any= useState(""); // Estado para el valor del input
-
+  const [isFirstLogin, setIsFirstLogin] = useState(false);
 
   useEffect(() => {
     if (!isLoading && !isAuthenticated) {
@@ -36,6 +39,18 @@ const Profile = () => {
     if (refScreen.current) {
       refScreen.current.style.maxHeight = `${window.innerHeight}px`;
     }
+
+    const checkFirstLogin = async () => {
+      console.log(user)
+      try {
+        const response = await axios.get(apiUrl+'check_first_login');
+        setIsFirstLogin(response.data.isFirstLogin);
+      } catch (error) {
+        console.error('Error fetching first login status:', error);
+      }
+    };
+
+    checkFirstLogin();
   }, []); 
 
   if (isLoading) {
@@ -46,15 +61,34 @@ const Profile = () => {
     return null; // El usuario será redirigido al login automáticamente
   }
 
+
+
   const handleSave = () => {
     console.log("Valor ingresado:", inputValue);
     onClose(); // Cierra el diálogo
   };
-
+  const asd = async () => {
+    const token = await getAccessTokenSilently();
+    console.log(isAuthenticated)
+    axios.get(apiUrl+'check_first_login',{
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    })
+    .then((res) => {
+      // Guardar la respuesta en el estado
+      console.log(res.data.message);
+    })
+    .catch((error) => {
+      console.error("Hubo un error al hacer la solicitud:", error);
+    });
+  }
 
   return (
     <div ref={refScreen}>
       <BaseCompents>
+      <Button onClick={asd}>asd</Button>
+
         <GridItem area={'nav'}  rowSpan={7} colSpan={5}>
           <CardRoot margin={5} height={'100%'}>
             { !false ? <> 
@@ -67,7 +101,7 @@ const Profile = () => {
           </CardRoot>
         </GridItem>
       </BaseCompents>
-      <ProfileInfoDialog open={true}/>
+      <ProfileInfoDialog open={false}/>
     </div>
   )
 }
