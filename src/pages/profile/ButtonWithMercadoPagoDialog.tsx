6536cea415cfdf4456d24a49ed6c25407ccbf6e9
gonza <gonzalo.muscolo@gmail.com>
2025-02-07 00:30:57 -0300

    adding mercadopago code

--- src/pages/profile/ButtonWithMercadoPagoDialog.tsx
+++ src/pages/profile/ButtonWithMercadoPagoDialog.tsx
@@ -1,17 +1,23 @@
 import { Button, Modal, ModalContent, ModalOverlay} from "@chakra-ui/react"
 import { initMercadoPago, CardPayment } from "@mercadopago/sdk-react"
 import { useState } from "react"
+import { putSubscription } from "../../services/user"
+import { useAuth0 } from "@auth0/auth0-react"
 
-
-initMercadoPago('TEST-28d1010f-acc4-474a-992d-861df9701807')
+initMercadoPago('APP_USR-5cadb7a0-b3cf-4da1-8490-d1a4ff48d49b')
 
 const ButtonWithMercadoPagoDialog = () => {
   const [isOpen, setIsOpen] = useState(false)
+  const { isAuthenticated, loginWithRedirect, user, isLoading, getAccessTokenSilently } = useAuth0();
 
   const pay = async (paymentInfo:any) => {
-      console.log(paymentInfo)
-      
+    if(user?.email){
+      const token = await getAccessTokenSilently();
+      console.log(user)    
+      putSubscription(token, user.email, paymentInfo)
     }
+  }
+
   return(
     <>
       <Button onClick={() => setIsOpen(true)}>Pagar</Button>
---
+++ src/services/user.ts
@@ -0,0 +1,20 @@
+import axios from 'axios';
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL;
+
+export const putSubscription = async (token : string,userEmail:string, values: any) => {
+    try {
+      const response = await axios.post(
+        `${API_BASE_URL}users/${userEmail}/subscribe`,
+        values,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`
+          }
+        }
+      );
+      return response.data;
+    } catch (error) {
+      console.error('Error creating subscription:', error);
+      throw error;
+    }
+}
