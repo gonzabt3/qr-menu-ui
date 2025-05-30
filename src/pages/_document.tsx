bcb251c82190bb815be99967cf95c2af6dd3e52b
gonza <gonzalo.muscolo@gmail.com>
2025-05-30 13:21:58 -0300

    adding google code

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,22 @@
-import { Html, Head, Main, NextScript } from "next/document";
-import React from "react";
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Google Tag Manager */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-519915198"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'AW-519915198');
+            `,
+          }}
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
